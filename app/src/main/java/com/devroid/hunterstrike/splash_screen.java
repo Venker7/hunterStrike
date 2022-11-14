6e9dfd61969dd96c69489c6e457be6c06020dccf
@venkernong1 <venkernong1@gmail.com>
2022-11-14 14:24:18 +0530

    Initial commit

---
+++ app/src/main/java/com/devroid/hunterstrike/splash_screen.java
@@ -0,0 +1,14 @@
+package com.devroid.hunterstrike;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+    }
+}

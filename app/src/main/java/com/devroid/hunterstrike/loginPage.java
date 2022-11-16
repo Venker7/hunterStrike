8e527526cc742a85ff88cb6e3152b37a11dc9462
@venkernong1 <venkernong1@gmail.com>
2022-11-16 12:05:48 +0530

    new update 2.0

--- app/src/main/java/com/devroid/hunterstrike/loginPage.java
+++ app/src/main/java/com/devroid/hunterstrike/loginPage.java
@@ -2,13 +2,28 @@ package com.devroid.hunterstrike;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class loginPage extends AppCompatActivity {
-
+    private Button move2;
+    public static String PREFS_NAME="MyPrefsFile";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        SharedPreferences sharedPreferences = getSharedPreferences(loginPage.PREFS_NAME,0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+       move2=findViewById(R.id.sign_up);
+        move2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(loginPage.this,Registration.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/devroid/hunterstrike/splash_screen.java
+++ app/src/main/java/com/devroid/hunterstrike/splash_screen.java
@@ -2,13 +2,36 @@ package com.devroid.hunterstrike;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class splash_screen extends AppCompatActivity {
-
+    private static int SPLASHSCREEN=2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
+        setTitle(" ");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences sharedPreferences = getSharedPreferences(loginPage.PREFS_NAME,0);
+                boolean hasLoggedIn = sharedPreferences.getBoolean("hasLoggedIn",false);
+                if(hasLoggedIn)
+                {
+                    Intent intent = new Intent(splash_screen.this,Registration.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else
+                {
+                    Intent intent = new Intent(splash_screen.this,loginPage.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        },SPLASHSCREEN);
     }
 }

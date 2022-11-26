15a33ec2adc191f1cfc2d908ba0e916a1e19db4b
@venkernong1 <venkernong1@gmail.com>
2022-11-26 10:21:39 +0530

    new update 2.6

--- app/src/main/java/com/devroid/hunterstrike/Donor.java
+++ app/src/main/java/com/devroid/hunterstrike/Donor.java
@@ -3,12 +3,28 @@ package com.devroid.hunterstrike;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class Donor extends AppCompatActivity {
-
+String[] type = {"A+","A-","B+","B-","AB+","AB-","0+","O-"};
+AutoCompleteTextView autoCompleteTextView;
+ArrayAdapter<String> adapterTypes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donor);
+        autoCompleteTextView = findViewById(R.id.auto_text);
+        adapterTypes = new ArrayAdapter<String>(this,R.layout.blood_group,type);
+        autoCompleteTextView.setAdapter(adapterTypes);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String type = adapterTypes.getItem(i).toString();
+
+            }
+        });
     }
 }
